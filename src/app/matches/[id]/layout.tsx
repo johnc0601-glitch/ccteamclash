b19a592546f5fea5119470305b28196d62e65e80
CFDTracker <johnc0601@gmail.com>
2026-08-25 08:55:36 -0400

    Add stable Matchday theme scope

--- src/app/matches/[id]/layout.tsx
+++ src/app/matches/[id]/layout.tsx
@@ -1,5 +1,5 @@
 import './matchday-theme.css';
 
 export default function MatchdayLayout({children}: Readonly<{children: React.ReactNode}>) {
-  return children;
+  return <div data-matchday-root>{children}</div>;
 }
