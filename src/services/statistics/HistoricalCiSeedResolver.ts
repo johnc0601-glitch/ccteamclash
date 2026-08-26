f01b53359ccc324919869dd008a8f510576411d6
CFDTracker <johnc0601@gmail.com>
2026-08-26 12:52:44 -0400

    Use canonical Absher player ID for historical CI seed

--- src/services/statistics/HistoricalCiSeedResolver.ts
+++ src/services/statistics/HistoricalCiSeedResolver.ts
@@ -98,7 +98,7 @@ const CONFIRMED_HISTORICAL_SEED_OVERRIDES = new Map<string, number>([
   ['coastal-clash-2025-2026:lizzie-goddard', 700],
   ['coastal-clash-2025-2026:logan-hitchcock', 850],
   ['coastal-clash-2025-2026:lucian-odom', 875],
-  ['coastal-clash-2024-2025:stephen-ajov', 940],
+  ['coastal-clash-2024-2025:stev-absher', 940],
   ['coastal-clash-2025-2026:christopher-king-jr', 963],
   ['coastal-clash-2025-2026:sarah-moore', 700],
   ['coastal-clash-2025-2026:will-lindsey', 900],
