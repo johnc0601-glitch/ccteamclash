7ee718931c73ceed72977c51af9f06479eb19541
CFDTracker <johnc0601@gmail.com>
2026-08-26 11:52:29 -0400

    Add reviewed Ray Ledbetter historical CI seed

--- src/services/statistics/HistoricalCiSeedResolver.ts
+++ src/services/statistics/HistoricalCiSeedResolver.ts
@@ -31,6 +31,8 @@ const CONFIRMED_HISTORICAL_SEED_OVERRIDES = new Map<string, number>([
   ['coastal-clash-2025-2026:john-loyd', 900],
   ['coastal-clash-2024-2025:thomas-vaughn', 900],
   ['coastal-clash-2025-2026:thomas-vaughn', 900],
+  ['coastal-clash-2024-2025:ray-ledbetter', 900],
+  ['coastal-clash-2025-2026:ray-ledbetter', 900],
 ]);
 
 /**
