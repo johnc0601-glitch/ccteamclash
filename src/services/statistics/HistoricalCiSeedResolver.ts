887e4b1cc079bd318e273e5a65df76235f80589c
CFDTracker <johnc0601@gmail.com>
2026-08-26 11:54:44 -0400

    Add reviewed 2025-26 CI seed overrides

--- src/services/statistics/HistoricalCiSeedResolver.ts
+++ src/services/statistics/HistoricalCiSeedResolver.ts
@@ -33,6 +33,9 @@ const CONFIRMED_HISTORICAL_SEED_OVERRIDES = new Map<string, number>([
   ['coastal-clash-2025-2026:thomas-vaughn', 900],
   ['coastal-clash-2024-2025:ray-ledbetter', 900],
   ['coastal-clash-2025-2026:ray-ledbetter', 900],
+  ['coastal-clash-2025-2026:aj-lehmann', 850],
+  ['coastal-clash-2025-2026:peter-hourigan', 850],
+  ['coastal-clash-2025-2026:hunter-gainey', 900],
 ]);
 
 /**
