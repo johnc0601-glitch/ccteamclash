7018bf99950f2389c1926b9cc6036f14455837bb
CFDTracker <johnc0601@gmail.com>
2026-08-31 22:10:57 -0400

    Add HEIC converter type declaration

---
+++ src/types/heic-convert.d.ts
@@ -0,0 +1,10 @@
+declare module 'heic-convert' {
+  type HeicConvertOptions = {
+    buffer: Buffer | Uint8Array;
+    format: 'JPEG' | 'PNG';
+    quality?: number;
+  };
+
+  function convert(options: HeicConvertOptions): Promise<Uint8Array>;
+  export default convert;
+}
