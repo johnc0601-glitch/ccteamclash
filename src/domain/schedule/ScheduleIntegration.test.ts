8cf42d88d1becad815d71df4cdb06b75ee17131c
CFDTracker <johnc0601@gmail.com>
2026-09-07 10:30:59 -0400

    Use course test fixture in schedule integration tests

--- src/domain/schedule/ScheduleIntegration.test.ts
+++ src/domain/schedule/ScheduleIntegration.test.ts
@@ -1,6 +1,5 @@
 import assert from 'node:assert/strict';
 import test from 'node:test';
-import {MockCourseRepository} from '@/domain/course/CourseRepository';
 import {SCHEDULE_IMPORT_SCHEMA_VERSION} from '@/domain/schedule/ScheduleImport';
 import {MockScheduleRepository} from '@/domain/schedule/ScheduleRepository';
 import {ScheduleService} from '@/domain/schedule/ScheduleService';
@@ -8,6 +7,7 @@ import {MockSeasonRepository} from '@/domain/season/SeasonRepository';
 import {SeasonService} from '@/domain/season/SeasonService';
 import {MockTeamRepository} from '@/repositories/TeamRepository';
 import {TeamService} from '@/services/TeamService';
+import {MockCourseRepository} from '@/test-fixtures/MockCourseRepository';
 
 function createService() {
   const repository = new MockScheduleRepository();
