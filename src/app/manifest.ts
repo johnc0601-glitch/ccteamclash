842d3a471fd1508cda94766cacff044038d753c0
CFDTracker <johnc0601@gmail.com>
2026-09-25 11:38:50 -0400

    Add maskable PWA icon declaration

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -20,6 +20,12 @@ export default function manifest(): MetadataRoute.Manifest {
         type: 'image/svg+xml',
         purpose: 'any',
       },
+      {
+        src: '/pwa/team-clash-app-icon.svg',
+        sizes: 'any',
+        type: 'image/svg+xml',
+        purpose: 'maskable',
+      },
     ],
     shortcuts: [
       {
